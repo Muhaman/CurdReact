f689d2a2cf9f45fff2cb50c47c37922073fc6408
Muhaman Ahmed <muhaman78@gmail.com>
2020-01-08 16:40:27 +0500

    first commit

---
+++ build/precache-manifest.984e9efeaba3eefd23dca6ddd19d069b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7066bb0a7660010e287468d12427cc47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4eb689181bc0818c874",
+    "url": "/static/js/2.dc30c982.chunk.js"
+  },
+  {
+    "revision": "6b8a8ed29fc352ee81d4452089aa1005",
+    "url": "/static/js/2.dc30c982.chunk.js.LICENSE"
+  },
+  {
+    "revision": "318e95acb2456f1a0df2",
+    "url": "/static/js/main.b71956d1.chunk.js"
+  },
+  {
+    "revision": "b1378ffcc89b6a5b82ec",
+    "url": "/static/js/runtime-main.39a91fdc.js"
+  }
+]);
